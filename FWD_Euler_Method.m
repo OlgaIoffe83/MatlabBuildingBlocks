827275cc9e89d2d75af6790bb354494f03615722
OlgaIoffe83 <150229553+OlgaIoffe83@users.noreply.github.com>
2025-01-21 15:24:20 +0200

    Add files via upload
    Renamed the function and updated the documentation

--- FWD_Euler_Method.m
+++ FWD_Euler_Method.m
@@ -1,11 +1,16 @@
-% function int_step_square
+% function FWD_Euler_Method
+% Implementation of the simplest numerical integration - Forward Euler
+% Method. 
+% https://en.wikipedia.org/wiki/Explicit_and_implicit_methods
+% x(i+1) = x(i) + dt * xdot(i)
+% this function supports vectorial notation. 
 % input: xi - current state vector
 %        xi_dot - current state derivative vector
 %        dt - integration step
 % output: xi_p1 - next state vector
 
 
-function xi_p1 = int_step_square(xi,xi_dot, dt)
+function xi_p1 = FWD_Euler_Method(xi,xi_dot, dt)
 
 xi_p1 = xi + xi_dot*dt;
 
